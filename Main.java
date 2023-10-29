6cfd55dcf65991fe8b670f40e29d4cdc89c67d4f
Karthick_Sivasamy <91825246+KarthickSivasamy@users.noreply.github.com>
2023-10-29 12:39:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riskassesment;
+
+/**
+ *
+ * @author Elcot
+ */
+public class Main {
+    public static void main(String[] args) throws Exception
+    {                          
+        MainFrame cf=new MainFrame();
+        cf.setTitle("Main Frame");
+        cf.setVisible(true);
+        cf.setResizable(false);	        
+    }
+}
